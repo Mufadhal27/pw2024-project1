b28911c1499944c1fb7e0d99f0571f97c741f193
Mufadhal27 <tmufadhal@gmail.com>
2024-05-10 22:10:19 +0700

    first commit

---
+++ index/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>Halaman Login</title>
+</head>
+<?php
+if(isset($_POST['submit'])){
+    require_once 'dbkoneksi.php';
+
+    $user = $dbh->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+    $user->execute([
+        $_POST['email'], $_POST['password'],
+    ]);
+
+    $count = $user->rowCount();
+
+    if($count > 0) {
+        session_start();
+    
+        $_SESSION['user'] = $user->fetch();
+        header("location:index.php"); 
+    } else {
+        header("location:login.php");  
+    }
+}
+?>
+
+<body>
+    <div class="container mt-5">
+        <h2 class="text-center">Login</h2>
+        <form action=" " method="POST">
+            <div class="form-group row">
+                <label for="email" class="col-4 col-form-label">Email</label>
+                <div class="col-8">
+                    <input id="email" name="email" type="email" class="form-control" requered>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-4 col-form-label">Password</label>
+                <div class="col-8">
+                    <input id="password" name="password" type="password" class="form-control" requered>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
+</html>
